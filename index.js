2b5066970676b30afdec474ee9ab98a944e8ec51
Nikita <khakimov.nikita.vch@gmail.com>
2023-01-24 21:05:30 +0200

    variabels

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const authRouter = require('./authRouter');
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -13,7 +12,7 @@ app.use('/auth', authRouter);
 const start = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://admin:hHuIK9la4L9wtg19@cluster0.tl6h4xw.mongodb.net/auth_roles?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.tl6h4xw.mongodb.net/auth_roles?retryWrites=true&w=majority`
     );
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (err) {
