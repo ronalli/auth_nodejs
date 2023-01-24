1cc1cc75de03413e4dffea59e5b1bc3aed1f8a79
Nikita <khakimov.nikita.vch@gmail.com>
2023-01-24 21:59:37 +0200

    mongoose

--- authController.js
+++ authController.js
@@ -1,9 +1,28 @@
+const User = require('./models/User');
+const Role = require('./models/Role');
+
 class authController {
-  async registration(req, res) {}
+  async registration(req, res) {
+    try {
+    } catch (err) {
+      res.status(400).json({ message: '' });
+    }
+  }
 
-  async login(req, res) {}
+  async login(req, res) {
+    try {
+    } catch (err) {
+      res.status(400).json({ message: '' });
+    }
+  }
 
-  async getUsers(req, res) {}
+  async getUsers(req, res) {
+    try {
+      res.json('server work');
+    } catch (err) {
+      res.status(400).json({ message: '' });
+    }
+  }
 }
 
-module.exports = authController;
+module.exports = new authController();
--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const authRouter = require('./authRouter');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const URL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.tl6h4xw.mongodb.net/auth_roles?retryWrites=true&w=majority`;
 
 app.use(express.json());
 app.use('/auth', authRouter);
 
 const start = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.tl6h4xw.mongodb.net/auth_roles?retryWrites=true&w=majority`
-    );
+    mongoose
+      .connect(URL)
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((err) => console.log(`DB connection error: ${err}`));
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (err) {
     console.log(err);
