5668f692b816e6f6005362dc6e0fd9935c23cead
Nikita <khakimov.nikita.vch@gmail.com>
2023-01-25 23:24:17 +0200

    registrationAndValidation

--- authController.js
+++ authController.js
@@ -1,18 +1,40 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 class authController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Registration error', errors });
+      }
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        res.status(400).json({ message: 'User with this name exists' });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: 'USER' });
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
+      return res.json({ message: 'User successfully registered' });
     } catch (err) {
-      res.status(400).json({ message: '' });
+      console.log(err);
+      res.status(400).json({ message: 'Registration error' });
     }
   }
 
   async login(req, res) {
     try {
     } catch (err) {
-      res.status(400).json({ message: '' });
+      console.log(err);
+      res.status(400).json({ message: 'Login error' });
     }
   }
 
@@ -20,6 +42,7 @@ class authController {
     try {
       res.json('server work');
     } catch (err) {
+      console.log(err);
       res.status(400).json({ message: '' });
     }
   }
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,19 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
+const { check } = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post(
+  '/registration',
+  [
+    check('username', 'Name must not be empty').notEmpty(),
+    check(
+      'password',
+      'Password greater than 4 and less than 10 characters'
+    ).isLength({ min: 4, max: 10 }),
+  ],
+  controller.registration
+);
 router.post('/login', controller.login);
 router.get('/users', controller.getUsers);
 
